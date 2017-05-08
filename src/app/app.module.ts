d6c383b355e6d5f61c8aa20821deea87a0dada9a
Frédéric DUPERIER <fduperier@gmail.com>
2017-05-08 16:50:35 +0200

    Directive : Unless

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AutoGrowDirective } from './shared/auto-grow.directive';
+import { UnlessDirective } from './shared/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AutoGrowDirective
+    AutoGrowDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
